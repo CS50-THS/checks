ac19f3782fb5709b959c97ef85ce26747772f739
Bill Soistmann <bsoist@users.noreply.github.com>
2024-10-02 15:33:59 -0400

    Update __init__.py

--- sort/__init__.py
+++ sort/__init__.py
@@ -14,16 +14,4 @@ def answers():
     if "TODO" in content:
         raise check50.Failure("Not all questions answered.")
 
-@check50.check(exists)
-def sorts():
-    """correctly identifies each sort"""
-
-    check50.log("checking that sorts are classified correctly...")
-
-    expected = ["sort1 uses:\s*[Bb][Uu][Bb][Bb][Ll][Ee]", "sort2 uses:\s*[Mm][Ee][Rr][Gg][Ee]", "sort3 uses:\s*[Ss][Ee][Ll][Ee][Cc][Tt][Ii][Oo][Nn]"]
-    actual = open("answers.txt", "r").read()
-
-    for e in expected:
-        if not search(e, actual):
-            raise check50.Failure("Incorrect assignment of sorts.")
 
