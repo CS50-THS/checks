3a2e129d0b22ec32bee57237ee68060792ec9d47
bsoist <bill@soistmann.com>
2025-04-02 10:00:13 -0400

    Add check for Counter

---
+++ counter/__init__.py
@@ -0,0 +1,18 @@
+import check50
+
+@check50.check()
+def index_exists():
+    """index.html exists"""
+    check50.exists("index.html")
+
+@check50.check()
+def style_exists():
+    """style.css exists"""
+    check50.exists("style.css")
+
+@check50.check()
+def script_exists():
+    """script.js exists"""
+    check50.exists("script.js")
+
+   
